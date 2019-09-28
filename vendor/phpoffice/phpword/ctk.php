a5ad1f090fc794f7a4347d0b65734c66710d6aeb
yogagrmy99 <yoga_doom99@yahoo.co.id>
2019-09-29 01:19:39 +0700

    Revert "Cetak jadi"
    This reverts commit 3eb0e905b74fc421e60859ce0ac04cfde342b0e9.

--- vendor/phpoffice/phpword/ctk.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'bootstrap.php';
-
-$template = new TemplateProcessor(‘ganti.docx’);
-    $template->setValue('[ganti]', 'John Doe');
-$objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
-$objWriter->save('helloWorld.docx');
