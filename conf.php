45b9c6ecad5f3123e713224ef82b34f8b1151e89
akhmadarief <w.akhmadarief@gmail.com>
2019-10-02 12:18:15 +0700

    Update conf.php

--- conf.php
+++ conf.php
@@ -3,9 +3,11 @@
 $server = "localhost";
 $user = "root";
 $password = "";
-$db_name = "yo";
-$db = mysqli_connect($server, $user, $password, $db_name);
-if( !$db ){
+$db_name = "icai2019";
+$conn = mysqli_connect($server, $user, $password, $db_name);
+//$db = $conn;
+
+if(!$conn){
     die("Gagal terhubung dengan database: " . mysqli_connect_error());
 }
 
